4aefb1d0f89ece87100c650fad188c4789d31492
Cheerios <138498145+cheerios4316@users.noreply.github.com>
2026-06-19 16:26:06 +0200

    .
    .

--- src/easy-docker-deploy.ts
+++ src/easy-docker-deploy.ts
@@ -8,15 +8,3 @@ const { spawn } = require('child_process');
 const command = extractCommand(process.argv);
 
 runCommand(command);
-
-//const action = process.argv[2];
-
-
-/*const childProcess = spawn("echo stronzooo", {
-    stdio: 'inherit',
-    shell: true,
-});
-
-childProcess.on('exit', (exitCode: any) => {
-    process.exit(exitCode ?? 1);
-});*/
